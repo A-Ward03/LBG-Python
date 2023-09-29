8c66b171c6bb9107ea3d56bcd20154be713a604a
A-Ward03 <Andrew.mac.ward03@gmail.com>
2023-09-29 11:05:27 +0100

    edited run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 ```
 This file is removing removing the container, removing the image then creating both
 ```
-docker rm -f python-app
+docker rm -f LBG-Python
 
 docker rmi amward03/python-app
 
 docker build -t amward03/python-app .
 
-docker run -d -p 80:8080  --name python-app amward03/python-app:latest
+docker run -d -p 80:8080  --name LBG-Python amward03/python-app:latest
 
  echo "done"
