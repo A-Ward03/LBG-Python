1e1ab94234e715cc8e302d01edeb1be9912822a9
A-Ward03 <Andrew.mac.ward03@gmail.com>
2023-09-29 11:10:07 +0100

    edited run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 ```
 This file is removing removing the container, removing the image then creating both
 ```
+git add .
+
+read -p "Enter commit message: " commit_message
+
+git commit -m "$commit_message"
+
+git push
+
+echo "changes added to repo"
+
 docker rm -f LBG-Python
 
 docker rmi amward03/python-app
@@ -8,5 +18,7 @@ docker rmi amward03/python-app
 docker build -t amward03/python-app .
 
 docker run -d -p 80:8080  --name LBG-Python amward03/python-app:latest
+
+open http://localhost:8080/index.html 
 
  echo "done"
